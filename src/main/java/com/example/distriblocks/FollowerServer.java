a33326860f5e9279d9a352fc4c7b725db64482a9
arnaud <arnaud1.brisset@gmail.com>
2025-10-23 12:05:01 +0800

    followerServer class

--- src/main/java/com/example/distriblocks/FollowerServer.java
+++ src/main/java/com/example/distriblocks/FollowerServer.java
@@ -1,1 +1,20 @@
 // contains static information + operations + specific implementations which the client can perform
+
+public class FollowerServer implements Server {
+    public boolean askMasterToLock(String lockName, int clientID) throws IOException{
+        // connect with leader
+        // detail of request...
+        // returns: True if successful, False if not
+    }
+
+    public boolean askMasterToRelease(String lockName, int clientID) throws IOexception{
+        // connect with leader
+        // detail of request: ...
+        // returns: True if successful, False if not
+    }
+
+    boolean connectLeader(String info) throws IOException{
+        // get IP address of leader server
+        // returns: True if successful, False if not
+    }
+}
